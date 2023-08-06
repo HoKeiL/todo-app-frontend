ae05a125e851eb120e7c4412a3c93b91c773490c
Your Name <you@example.com>
2023-08-06 16:49:30 +0100

    added input to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { todoList } from "./utils/todoAppTitle";
+import { todoList } from "./coponents/todoAppTitle";
+import { todoInput } from "./coponents/todoInput";
+import "./App.css";
 
 function App(): JSX.Element {
   return (
     <>
       <h1 className="title">{todoList("My ToDo App")}</h1>
+      <p>{todoInput()}</p>
     </>
   );
 }
