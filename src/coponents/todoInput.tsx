4004617e280a339aa1b6b54bc6100e319bbc2347
Your Name <you@example.com>
2023-08-07 13:09:04 +0100

    todo app layout

--- src/coponents/todoInput.tsx
+++ src/coponents/todoInput.tsx
@@ -1,7 +1,7 @@
 export function todoInput(): JSX.Element {
   return (
     <>
-      <input placeholder="Add new task..." />
+      <input className="inputBar" placeholder="Add new task..." />
       <button className="button">Add</button>
     </>
   );
