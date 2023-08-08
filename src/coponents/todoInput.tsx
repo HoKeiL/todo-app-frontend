2e29d40fc0bd1750ffc6be23981ef3425bc2aa25
Your Name <you@example.com>
2023-08-08 13:07:45 +0100

    added imput date

--- src/coponents/todoInput.tsx
+++ src/coponents/todoInput.tsx
@@ -1,7 +1,10 @@
 export function todoInput(): JSX.Element {
+  
   return (
     <>
       <input className="inputBar" placeholder="Add new task..." />
+      <p className="due-date-text"> Due date: </p>
+      <input className="due-date" type="date" />
       <button className="button">Add</button>
     </>
   );
