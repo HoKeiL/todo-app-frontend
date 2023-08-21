647a114c1b740c7659232857c61e5ae8d274360e
Your Name <you@example.com>
2023-08-21 23:35:15 +0100

    fetch task from api - get

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
+import dotenv from 'dotenv'
 import { todoList } from "./coponents/todoAppTitle";
-// import { todoCardProp, DisplayTodoTask } from "./coponents/todoTaskCard";
+import { todoCardProp, DisplayTodoTask } from "./coponents/todoTaskCard";
 import "./App.css";
 
 
 function App(): JSX.Element {
   const [isFirstLoad, setIsFirstLoad] = useState(true);
   const [message, setMessage] = useState<string>();
-  const [Task, setTask] = useState<string>('');
+  const [NewTask, setNewTask] = useState<string>('');
   const [DueDate, setDueDate] = useState<string>("");
+  const [TodosInProgress, setTodosInProgress] = useState<todoCardProp[]>([]);
 
 
-  const loadDataFromEndpoint = async (endpoint: `/${string}`) => {
+  const loadDataFromEndpoint = async (endpoint: `/todoapp`) => {
     try {
       const res = await fetch(`http://localhost:4000${endpoint}`);
       const body = await res.json();
@@ -28,37 +30,49 @@ function App(): JSX.Element {
   useEffect(() => {
     // safe to ignore exhaustive deps warning as we're _not_ triggering infinite updates, since our setState is conditional and not executed on all rerenders after the first one
     if (isFirstLoad) {
+      console.log("fetched data")
+      fetchNewTodos()
       // populate data on first load
       loadDataFromEndpoint("/todoapp");
       setIsFirstLoad(false);
+
     }
   });
 
   //use effect triggered when add todo task button is clicked, adding new task to the main todo list
-
+  async function fetchNewTodos() {
+    const response = await axios.get("http://localhost:4000/todoapp");
+    const todos = response.data;
+    setTodosInProgress(todos);
+  }
 
   function todoInput(): JSX.Element {
 
 
     function handleClick() {
-      console.log("add " + Task + " and " + DueDate + " to to-dos list!")
+      axios.post('http://localhost:4000/todoapp', {
+        task: NewTask,
+        dueDate: DueDate,
+        done: false
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
     }
 
     const today = moment(new Date()).format("YYYY-MM-DD");
 
     return (
       <div className="innerInputBarSection">
-        <input className="inputBar" placeholder="Add new task..." value={Task} onChange={(event) => { setTask(event.target.value) }} />
-        <input className="dueDate" type="date" value={DueDate} min={today} onChange={(event) => { setDueDate(event.target.value) }} />
-
+        <input className="inputBar" placeholder="Add new task..." value={NewTask} onChange={(event) => { setNewTask(event.target.value) }} />
+        <input className="dueDateInput" type="date" value={DueDate} min={today} onChange={(event) => { setDueDate(event.target.value) }} />
         <button className="button" onClick={handleClick}>+</button>
       </div>
     );
   }
-
-
-
-
 
   return (
     <>
@@ -77,11 +91,9 @@ function App(): JSX.Element {
       <div className="container">
 
         <div className="toDoMainList">
-          <p>to Do Main List goes here</p>
-          displaying all todo tasks
-          {/* {newInProgressToDoArray.map((task) => (
-            <DisplayTodoTask todo={task} key={task.id} />
-          ))} */}
+          {TodosInProgress.map((e) => (
+            <DisplayTodoTask todo={e} key={e.id} />
+          ))}
 
         </div>
         <div className="done">
