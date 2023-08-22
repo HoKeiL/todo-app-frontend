33aed925e8e935fc07f1d1d46d1bfb26ff75712c
Your Name <you@example.com>
2023-08-22 13:12:43 +0100

    run format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 // import { DisplayTodoTask } from "./coponents/todoTaskCard";
 import { todoCardProp, ToDoViewProp } from "./coponents/interfaces";
 import "./App.css";
