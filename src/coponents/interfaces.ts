10e3c7ca4d1c192ad160d6cfbb542c52f6630dd3
Your Name <you@example.com>
2023-08-22 12:21:32 +0100

    formatted and linted

--- src/coponents/interfaces.ts
+++ src/coponents/interfaces.ts
@@ -1,11 +1,10 @@
 export interface todoCardProp {
-    id: number;
-    task: string;
-    dueDate: string;
-    status: "Done" | "InProgress";
+  id: number;
+  task: string;
+  dueDate: string;
+  status: "Done" | "InProgress";
 }
 
-
 export interface ToDoViewProp {
-    todo: todoCardProp
+  todo: todoCardProp;
 }
--- src/coponents/todoTaskCard.tsx
+++ src/coponents/todoTaskCard.tsx
@@ -1,44 +1,56 @@
 import axios from "axios";
 import { useState } from "react";
-import { todoCardProp } from "./interfaces"
-
+import { todoCardProp } from "./interfaces";
 
 interface ToDoViewProp {
-    todo: todoCardProp
+  todo: todoCardProp;
 }
 
 export function DisplayTodoTask(props: ToDoViewProp): JSX.Element {
-    const [isDone, setIsDone] = useState<boolean>(false)
+  const [isDone, setIsDone] = useState<boolean>(false); //need state to remember previous state
 
-    async function handleDoneCheckbox() {
-        const isChecked = !isDone
-        setIsDone(isChecked)
-        const todoId = props.todo.id;
-        if (isChecked === true) {
-            props.todo["status"] = "Done"
-        } else { props.todo["status"] = "InProgress" }
-        const response = await axios.patch(`http://localhost:4000/todoapp/${todoId}`, { status: props.todo.status })
-        console.log(response.data + "has been updated")
+  async function handleDoneCheckbox() {
+    const isChecked = !isDone;
+    setIsDone(isChecked);
+    const todoId = props.todo.id;
+    if (isChecked === true) {
+      props.todo["status"] = "Done";
+    } else {
+      props.todo["status"] = "InProgress";
     }
+    const response = await axios.patch(
+      `http://localhost:4000/todoapp/${todoId}`,
+      { status: props.todo.status }
+    );
+    console.log(response.data + "has been updated");
+  }
 
-    async function handleDelete() {
-        const todoId = props.todo.id
-        const response = await axios.delete(`http://localhost:4000/todoapp/${todoId}`)
-        console.log(response.data + "ID:" + todoId + " has been deleted")
+  async function handleDelete() {
+    const todoId = props.todo.id;
+    const response = await axios.delete(
+      `http://localhost:4000/todoapp/${todoId}`
+    );
+    console.log(response.data + "ID:" + todoId + " has been deleted");
+  }
 
-    }
-
-    return (
-        <div className="todoTask">
-            <h3 className="taskDescription">Task: {props.todo.task}</h3>
-            <div className="isDoneGroup" >
-                <p className="isDone" >Done</p>
-                <input type="checkbox" className="isDoneButton" checked={isDone} onChange={handleDoneCheckbox} />
-            </div>
-            <hr className="divider"></hr>
-            <h4 className="taskdueDate">Due date: {props.todo.dueDate}</h4>
-            <button type="button" className="bin" onClick={handleDelete}> bin </button>
-        </div>
-    )
-
+  return (
+    <div className="todoTask">
+      <h3 className="taskDescription">Task: {props.todo.task}</h3>
+      <div className="isDoneGroup">
+        <p className="isDone">Done</p>
+        <input
+          type="checkbox"
+          className="isDoneButton"
+          checked={isDone}
+          onChange={handleDoneCheckbox}
+        />
+      </div>
+      <hr className="divider"></hr>
+      <h4 className="taskdueDate">Due date: {props.todo.dueDate}</h4>
+      <button type="button" className="bin" onClick={handleDelete}>
+        {" "}
+        bin{" "}
+      </button>
+    </div>
+  );
 }
