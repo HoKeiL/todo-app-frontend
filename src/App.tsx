8c3bc2ff62f8941d358cc2c18dd0802a0116cda5
Your Name <you@example.com>
2023-08-24 03:44:30 +0100

    tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -105,10 +105,10 @@ function App(): JSX.Element {
       });
       console.log(
         response.data +
-        "ID:" +
-        todoId +
-        " has been updated to " +
-        props.todo.completed
+          "ID:" +
+          todoId +
+          " has been updated to " +
+          props.todo.completed
       );
       fetchAllTodos("/todoapp");
     }
